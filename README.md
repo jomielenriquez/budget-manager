004fd3e1e5fc38a10f6b59cb78eeec5b6918555d
Jomiel <enriquez.jliquigan@gmail.com>
2023-12-22 12:07:31 +0800

    testing

--- README.md
+++ README.md
@@ -68,3 +68,10 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+### Deploying to Github Pages
+
+1. Push the changes first.
+2. run ```npm run deploy -- -m "<comment>"```
--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import UseGoogleSheets  from 'use-google-sheets';
 
 function App() {
+  const { data, loading, error } = UseGoogleSheets({
+    apiKey: 'AIzaSyBFIcaHmn63Pfgss3u4aLltaJtDyc3QxN8',
+    sheetId: '17eDNmTk4A7ATPmrBBBl15h0IUSZuwdLC6q2x7A2Ya-A',
+    sheetsOptions: [{ id: 'Sheet1' }],
+  });
+
+  console.log(JSON.stringify(data));
+
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
